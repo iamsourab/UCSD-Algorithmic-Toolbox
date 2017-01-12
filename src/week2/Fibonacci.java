a4d8a991c83c960192c888aaf05bbe2ddd520ba6
Unknown <sourabh.ocean@gmail.com>
2017-01-12 21:47:43 +0530

    GCD

--- src/week2/Fibonacci.java
+++ src/week2/Fibonacci.java
@@ -1,19 +1,30 @@
-package week2;
+	package week2;
+	import java.util.Scanner;
 
-import java.util.Scanner;
+	public class Fibonacci {
+	  private static long calc_fib(int n) {
+		  int a=1,b=1,c=0;
+		  if(n==1 ||n==2)
+		  {
+			  return 1;
+		  }
+		  else
+		  {
+			  for(int i=0;i<n-2;i++)
+			  {
+				  c=a+b;
+				  b=a;
+				  a=c;
+			  }
+			  return c;  
+		  }
 
-public class Fibonacci {
-  private static long calc_fib(int n) {
-    if (n <= 1)
-      return n;
+	  }
 
-    return calc_fib(n - 1) + calc_fib(n - 2);
-  }
+	  public static void main(String args[]) {
+	    Scanner in = new Scanner(System.in);
+	    int n = in.nextInt();
 
-  public static void main(String args[]) {
-    Scanner in = new Scanner(System.in);
-    int n = in.nextInt();
-
-    System.out.println(calc_fib(n));
-  }
-}
+	    System.out.println(calc_fib(n));
+	  }
+	}
---
+++ src/week2/Fibonacci_last_digit.java
@@ -0,0 +1,33 @@
+package week2;
+
+import java.util.Scanner;
+
+public class Fibonacci_last_digit {
+
+  private static long calc_fib(int n) {
+	  int a=1,b=1,c=0;
+	  if(n==1 ||n==2)
+	  {
+		  return 1;
+	  }
+	  else
+	  {
+		  for(int i=0;i<n-2;i++)
+		  {
+			  c=a+b;
+			  c=c%10;
+			  b=a%10;
+			  a=c;
+		  }
+		  return c;  
+	  }
+
+  }
+
+  public static void main(String args[]) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+
+    System.out.println(calc_fib(n));
+  }
+}
